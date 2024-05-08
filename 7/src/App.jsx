3392c6b03c63eda10a12d14bc983c2e23d179a7a
Dai Fujihara @daipresents <daipresents@gmail.com>
2024-05-08 22:13:03 +0900

    Fixed 7-2

--- 7/src/App.jsx
+++ 7/src/App.jsx
@@ -1,8 +1,10 @@
-import { useState } from "react";
+import { useContext } from "react";
+import { AdminFlagContext } from "./components/providers/AdminFlagProvider";
 import { Card } from "./components/Card";
 
 export const App = () => {
-  const [isAdmin, setIsAdmin] = useState(false);
+  const { isAdmin, setIsAdmin } = useContext(AdminFlagContext);
+
   const onClickSwitch = () => setIsAdmin(!isAdmin);
 
   return (
@@ -10,7 +12,7 @@ export const App = () => {
       {/** 管理者フラグがtrueの時とそれ以外で文字を出し分け */}
       {isAdmin ? <span>管理者です</span> : <span>管理者以外です</span>}
       <button onClick={onClickSwitch}>切り替え</button>
-      <Card isAdmin={isAdmin} />
+      <Card />
     </div>
   )
 }
