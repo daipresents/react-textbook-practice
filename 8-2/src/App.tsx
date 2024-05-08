f2ca6b6f4e2c0eb87d41df68dabc73980b2eafbf
Dai Fujihara @daipresents <daipresents@gmail.com>
2024-05-08 23:29:28 +0900

    Fixed 8-2

---
+++ 8-2/src/App.tsx
@@ -0,0 +1,24 @@
+import { useState } from "react";
+import { ListItem } from "./component/ListItem";
+import userList from "./data/userList.json"
+
+type User = {
+  id: number;
+  name: string;
+  age: number;
+  personalColor: string;
+};
+
+function App() {
+  const [users, setUsers] = useState<User[]>(userList);
+
+  return (
+    <div>
+      {users.map(user => (
+        <ListItem id={user.id} name={user.name} age={user.age} />
+      ))}
+    </div>
+  );
+}
+
+export default App;
