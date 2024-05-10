3497cd4e22a68cd05080cb157ad7c69656be94de
Dai Fujihara <daipresents@gmail.com>
2024-05-10 14:16:45 +0900

    Fixed 8-5

--- 8-2/src/component/ListItem.tsx
+++ 8-2/src/component/ListItem.tsx
@@ -1,6 +1,7 @@
+import type { FC } from "react";
 import type { User } from "../types/user";
 
-export const ListItem = (props: User) => {
+export const ListItem: FC<User> = props => {
   const { id, name, age, personalColor } = props;
   return (
     <p style={{ color: personalColor }}>
